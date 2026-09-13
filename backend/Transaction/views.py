e2570ac37e45edc6d3f10ef3d2c51c84164c1b4a
ujjalmukthakim <ujjalahammed2143@gmail.com>
2026-09-13 10:21:16 +0600

    views.py

--- backend/Transaction/views.py
+++ backend/Transaction/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Transaction
+from .serializers import TransactionSerializer
 
 # Create your views here.
+
+api_view['POST']
+def TransactionViewSet(request):
+    serializer=TransactionSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response ('This is good')
+    return Response('something went wrong over here')
+
